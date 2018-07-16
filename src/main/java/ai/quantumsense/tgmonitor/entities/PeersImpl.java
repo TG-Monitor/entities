86d73075c9d6b15915e674d3e89ea3891ab233b3
Daniel Weibel <danielmweibel@gmail.com>
2018-07-16 17:33:10 +0800

    Make peers pull-based (like other entitities)
    This is for changing the way messages are read from one process per peer
    to a single process which reads all the messages and a thread that
    filters out the messages of those peers that are currently in Peers.

--- src/main/java/ai/quantumsense/tgmonitor/entities/PeersImpl.java
+++ src/main/java/ai/quantumsense/tgmonitor/entities/PeersImpl.java
@@ -8,11 +8,9 @@ import java.util.Set;
 public class PeersImpl implements Peers {
 
     private Set<String> peers = new LinkedHashSet<>();
-    private PeersUpdater updater;
 
-    public PeersImpl(PeersUpdater updater, ServiceLocator<Peers> peersLocator) {
+    public PeersImpl(ServiceLocator<Peers> peersLocator) {
         peersLocator.registerService(this);
-        this.updater = updater;
     }
 
     @Override
@@ -23,34 +21,25 @@ public class PeersImpl implements Peers {
     @Override
     public void setPeers(Set<String> peers) {
         this.peers = new LinkedHashSet<>(peers);
-        update();
     }
 
     @Override
     public void addPeer(String peer) {
         this.peers.add(peer);
-        update();
     }
 
     @Override
     public void addPeers(Set<String> peers) {
         this.peers.addAll(peers);
-        update();
     }
 
     @Override
     public void removePeer(String peer) {
         this.peers.remove(peer);
-        update();
     }
 
     @Override
     public void removePeers(Set<String> peers) {
         this.peers.removeAll(peers);
-        update();
-    }
-
-    private void update() {
-        updater.peersChanged(new LinkedHashSet<>(this.peers));
     }
 }
